2680789b3be73530adabf95ee16e406ee36bfb48
Swapnil Sarkar <swapnilmkab@gmail.com>
2023-01-13 21:59:47 +0530

    routes folder updated

---
+++ server-side/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: "./app.env" });
+const conn = require("./db");
+const allRoutes = require("./routes/all-route");
+const authRoutes = require("./routes/auth-route");
+const userRoutes = require("./routes/user-route");
+const doctorRoutes = require("./routes/doctor-route");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//routes
+app.use("/api", allRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+//default route
+app.get("/", (req, res) => {
+  res.send("Welcome to the hackathon-jan server v1.0.0!");
+});
+
+// Connection to database and starting the express server
+conn().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("Server is running on port: " + process.env.PORT);
+  });
+});
